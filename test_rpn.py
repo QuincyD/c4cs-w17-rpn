2c6a0011ca80510624cddd5f008e3a6bd983c267
Quincy D <quincyd@umich.edu>
2017-03-16 18:48:44 -0400

    added tests for multiply, divide, and exponentiation

--- test_rpn.py
+++ test_rpn.py
@@ -8,3 +8,15 @@ class TestBasics(unittest.TestCase):
 	def test_subtract(self):
 		result = rpn.calculate('5 3 -')
 		self.assertEqual(2, result)
+
+	def test_multiply(self):
+		result = rpn.calculate('5 2 *')
+		self.assertEqual(10, result)
+
+	def test_division(self):
+		result = rpn.calculate('6 2 /')
+		self.assertEqual(3, result)
+
+	def test_exponentiation(self):
+		result = rpn.calculate('2 3 ^')
+		self.assertEqual(8, result)
